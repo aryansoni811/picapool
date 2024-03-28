cc222ade860bbd654716194326c0a0bfc7bdd35c
Aryan Soni <arpitsoni811@gmail.com>
2024-03-28 23:26:50 +0530

    Initial React + Tailwind CSS app with Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold underline">My React Tailwind App</h1>
+      <p className="text-gray-700">Welcome to your new app!</p>
+    </div>
+  );
+}
+
+export default App;
