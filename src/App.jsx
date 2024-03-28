d4625edddfba89a7505ed6ea2c1e01979602b745
Aryan Soni <arpitsoni811@gmail.com>
2024-03-29 00:17:47 +0530

    started home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-
+import { Home } from "./pages/home.jsx";
 function App() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold underline">My React Tailwind App</h1>
-      <p className="text-gray-700">Welcome to your new app!</p>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
